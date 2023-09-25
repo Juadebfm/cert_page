8b18d465192eac7f00f07a57b27ff64e46149c33
Juadeb <juadebgabriel@gmail.com>
2023-09-25 16:08:30 +0100

    fox

--- pdf.js
+++ pdf.js
@@ -14,14 +14,15 @@ window.onload = function () {
     html2pdf().from(invoice).set(opt).save();
   });
   document.getElementById("download_cert").addEventListener("click", () => {
-    const invoice = this.document.getElementById("invoice");
+    const invoice = this.document.getElementById("invoice_cert");
     console.log(invoice);
     console.log(window);
     var opt = {
       filename: "myfile.pdf",
       image: { type: "png", quality: 0.98 },
       html2canvas: {
-        scale: 2,
+        scale: 2
+        ,
       },
       jsPDF: { unit: "in", format: "letter", orientation: "landscape" },
     };
